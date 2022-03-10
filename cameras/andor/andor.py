7fb99b002eecb8679ebebd92f95e4da26c608e4e
AHReedy <alexander.h.reedy@gmail.com>
2022-03-10 14:42:59 -0800

    Updating

--- cameras/andor/andor.py
+++ cameras/andor/andor.py
@@ -9,9 +9,10 @@ from astropy.io import fits
 from test_transfer_to_remote import transfer
 
 SITE_ROOT = os.path.abspath(os.path.dirname(__file__) + '/../..')
-#print(os.path.join(SITE_ROOT, '../../config', 'logging.json'))
-print(SITE_ROOT)
-with open(os.path.join(SITE_ROOT, 'config', 'logging.json')) as data_file:
+#with open(os.path.join(SITE_ROOT, 'config', 'logging.json')) as data_file:
+#    params = json.load(data_file)
+
+with open(os.path.join('config', 'logging.json')) as data_file:
     params = json.load(data_file)
 
 logger = logging.getLogger("andorLogger")
