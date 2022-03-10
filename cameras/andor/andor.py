577f7cb10e841724e312b5341a2a04a6f5e2ea05
AHReedy <alexander.h.reedy@gmail.com>
2022-03-10 14:44:21 -0800

    Updating

--- cameras/andor/andor.py
+++ cameras/andor/andor.py
@@ -12,7 +12,7 @@ SITE_ROOT = os.path.abspath(os.path.dirname(__file__) + '/../..')
 #with open(os.path.join(SITE_ROOT, 'config', 'logging.json')) as data_file:
 #    params = json.load(data_file)
 
-with open(os.path.join('config', 'logging.json')) as data_file:
+with open(os.path.join('/home/wintermute/Software/AndorPullTest/config', 'logging.json')) as data_file:
     params = json.load(data_file)
 
 logger = logging.getLogger("andorLogger")
