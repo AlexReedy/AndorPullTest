6d53360e1a6a58ce09554ecea05db2f3331ffc9a
AHReedy <alexander.h.reedy@gmail.com>
2022-04-21 13:07:35 -0700

    changed path to logging.json

--- cameras/andor/andor_server.py
+++ cameras/andor/andor_server.py
@@ -10,12 +10,12 @@ import andor
 
 SITE_ROOT = os.path.abspath(os.path.dirname(__file__)+'/../..')
 
-with open(os.path.join(SITE_ROOT, '../../config', 'logging.json')) as data_file:
-    params = json.load(data_file)
-
-
-#with open(os.path.join('/home/wintermute/Software/AndorPullTest/config', 'logging.json')) as data_file:
+#with open(os.path.join(SITE_ROOT, '../../config', 'logging.json')) as data_file:
 #    params = json.load(data_file)
+
+
+with open(os.path.join('/home/wintermute/Software/AndorPullTest/config', 'logging.json')) as data_file:
+    params = json.load(data_file)
 
 logger = logging.getLogger("ifu_cameraLogger")
 logger.setLevel(logging.DEBUG)
